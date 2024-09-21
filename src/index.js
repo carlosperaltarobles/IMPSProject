f3f5ef20c3a2441704b360e5c5ba68da553d6d5a
carlosperaltarobles <peraltacarlos30@gmail.com>
2024-09-21 10:50:37 -0600

    primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'),  () => {
+    console.log('Servidor iniciado en el puerto:', app.get('port'));
+});
